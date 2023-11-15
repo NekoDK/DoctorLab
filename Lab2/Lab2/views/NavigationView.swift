fc2381e7d26d8695db87d2ea57cd3ee1d157ad64
Nadezhda Chelyadinova <62635172+nadzyathedude@users.noreply.github.com>
2023-11-15 21:45:26 +0600

    NavigationView WIP

---
+++ Lab2/Lab2/views/NavigationView.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationView.swift
+//  Lab2
+//
+//  Created by nadzya on 15.11.2023.
+//
+
+import SwiftUI
+
+struct NavigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationView()
+}
