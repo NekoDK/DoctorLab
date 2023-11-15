2470261ac82848dae55cb0e691e93047d8e40a36
Nadezhda Chelyadinova <62635172+nadzyathedude@users.noreply.github.com>
2023-11-15 20:55:35 +0600

    create OptionsView

---
+++ Lab2/Lab2/views/OptionsView.swift
@@ -0,0 +1,18 @@
+//
+//  OptionsView.swift
+//  Lab2
+//
+//  Created by nadzya on 15.11.2023.
+//
+
+import SwiftUI
+
+struct OptionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OptionsView()
+}
