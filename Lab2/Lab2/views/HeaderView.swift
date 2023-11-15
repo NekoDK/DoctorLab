b66a1baa6ee3b262b246604597f2eb469016c7ab
Nadezhda Chelyadinova <62635172+nadzyathedude@users.noreply.github.com>
2023-11-15 20:27:20 +0600

    add header view

---
+++ Lab2/Lab2/views/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  Lab2
+//
+//  Created by nadzya on 15.11.2023.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
